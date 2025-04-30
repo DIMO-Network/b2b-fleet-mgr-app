57f86d8e39a97b3bc7d8ed24534148324141d9cf
Mateusz Pohl <mateusz@dimo.zone>
2025-04-30 22:24:21 +0200

    use oracle instead of devices-api wip, cleanups

--- api/internal/controllers/vehicles.go
+++ api/internal/controllers/vehicles.go
@@ -3,17 +3,13 @@ package controllers
 import (
 	"bytes"
 	"fmt"
+	"github.com/DIMO-Network/b2b-fleet-mgr-app/internal/config"
+	"github.com/DIMO-Network/b2b-fleet-mgr-app/internal/service"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
 	"io"
 	"net/http"
 	"net/url"
-
-	"github.com/DIMO-Network/b2b-fleet-mgr-app/internal/config"
-	"github.com/DIMO-Network/b2b-fleet-mgr-app/internal/fleets"
-	"github.com/DIMO-Network/b2b-fleet-mgr-app/internal/service"
-	"github.com/friendsofgo/errors"
-	"github.com/gofiber/fiber/v2"
-	"github.com/rs/zerolog"
-	"github.com/tidwall/sjson"
 )
 
 type VehiclesController struct {
@@ -28,60 +24,6 @@ func NewVehiclesController(settings *config.Settings, logger *zerolog.Logger) *V
 		logger:      logger,
 		identityAPI: service.NewIdentityAPIService(*logger, settings.IdentityAPIURL.String()),
 	}
-}
-
-func (v *VehiclesController) PostDevicesAPIFromVin(c *fiber.Ctx) error {
-	targetURL := v.settings.DevicesAPIURL.JoinPath("/v1/user/devices/fromvin")
-	setBytes, err := sjson.SetBytes(c.Body(), "preApprovedPSK", v.settings.CompassPreSharedKey)
-	if err != nil {
-		return err
-	}
-	return v.proxyRequest(c, targetURL, setBytes, false)
-}
-
-func (v *VehiclesController) PostDevicesAPIMint(c *fiber.Ctx) error {
-	udID := c.Params("userDeviceId", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/user/devices/%s/commands/mint", udID))
-	b := c.Body()
-	return v.proxyRequest(c, targetURL, b, false)
-}
-
-func (v *VehiclesController) PostDevicesAPISyntheticMint(c *fiber.Ctx) error {
-	udID := c.Params("userDeviceId", "")
-	integrationID := c.Params("integrationId", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/user/devices/%s/integrations/%s/commands/mint", udID, integrationID))
-	b := c.Body()
-	return v.proxyRequest(c, targetURL, b, false)
-}
-
-func (v *VehiclesController) GetDevicesAPISyntheticMint(c *fiber.Ctx) error {
-	udID := c.Params("userDeviceId", "")
-	integrationID := c.Params("integrationId", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/user/devices/%s/integrations/%s/commands/mint", udID, integrationID))
-
-	return v.proxyRequest(c, targetURL, nil, false)
-}
-
-func (v *VehiclesController) GetDevicesAPIMint(c *fiber.Ctx) error {
-	udID := c.Params("userDeviceId", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/user/devices/%s/commands/mint", udID))
-
-	return v.proxyRequest(c, targetURL, nil, false)
-}
-
-func (v *VehiclesController) GetDevicesAPICompassVINLookup(c *fiber.Ctx) error {
-	vin := c.Params("vin", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/compass/device-by-vin/%s", vin))
-
-	return v.proxyRequest(c, targetURL, nil, true)
-}
-
-func (v *VehiclesController) PostDevicesAPIRegisterIntegration(c *fiber.Ctx) error {
-	udID := c.Params("userDeviceId", "")
-	integrationID := c.Params("integrationId", "")
-	targetURL := v.settings.DevicesAPIURL.JoinPath(fmt.Sprintf("/v1/user/devices/%s/integrations/%s", udID, integrationID))
-	b := c.Body()
-	return v.proxyRequest(c, targetURL, b, false)
 }
 
 func (v *VehiclesController) GetVehicleFromOracle(c *fiber.Ctx) error {
@@ -101,6 +43,30 @@ func (v *VehiclesController) RegisterVehicle(c *fiber.Ctx) error {
 	targetURL := v.settings.OracleAPIURL.JoinPath("/v1/vehicle/register")
 	b := c.Body()
 	return v.proxyRequest(c, targetURL, b, false)
+}
+
+func (v *VehiclesController) GetVehiclesVerificationStatus(c *fiber.Ctx) error {
+	vins := c.Query("vins", "")
+	targetURL := v.settings.OracleAPIURL.JoinPath("/v1/vehicle/verify")
+	targetURL.RawQuery = fmt.Sprintf("vins=%s", vins)
+	return v.proxyRequest(c, targetURL, nil, false)
+}
+
+func (v *VehiclesController) SubmitVehiclesVerification(c *fiber.Ctx) error {
+	targetURL := v.settings.OracleAPIURL.JoinPath("/v1/vehicle/verify")
+	return v.proxyRequest(c, targetURL, c.Body(), false)
+}
+
+func (v *VehiclesController) GetVehiclesMintData(c *fiber.Ctx) error {
+	vins := c.Query("vins", "")
+	targetURL := v.settings.OracleAPIURL.JoinPath("/v1/vehicle/mint")
+	targetURL.RawQuery = fmt.Sprintf("vins=%s", vins)
+	return v.proxyRequest(c, targetURL, nil, false)
+}
+
+func (v *VehiclesController) SubmitVehiclesMintData(c *fiber.Ctx) error {
+	targetURL := v.settings.OracleAPIURL.JoinPath("/v1/vehicle/mint")
+	return v.proxyRequest(c, targetURL, c.Body(), false)
 }
 
 func (v *VehiclesController) proxyRequest(c *fiber.Ctx, targetURL *url.URL, requestBody []byte, useCompassPSK bool) error {
@@ -166,93 +132,4 @@ func (v *VehiclesController) proxyRequest(c *fiber.Ctx, targetURL *url.URL, requ
 
 	// return the exact same JSON response
 	return c.Send(body)
-}
-
-// AddVehicles
-// @Summary Add vehicle(s) by VIN
-// @Description Adds a vehicle by VIN - adds to fleet data vendor, decodes, mints, sacd, synthetic
-// @Tags Vehicles
-// @Accept json
-// @Produce json
-// @Param  tokenId path int true "token Id of the vehicle NFT"
-// @Success 200
-// @Security     BearerAuth
-// @Router /v1/vehicles [post]
-func (v *VehiclesController) AddVehicles(c *fiber.Ctx) error {
-	payload := AddVehicleRequest{}
-	err := c.BodyParser(&payload)
-	if err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, errors.Wrap(err, "Invalid request body").Error())
-	}
-
-	// validation
-	for _, vin := range payload.VINs {
-		if len(vin) != 17 {
-			return fiber.NewError(fiber.StatusBadRequest, "Invalid vin")
-		}
-	}
-
-	compassSvc, err := fleets.NewCompassSvc(v.settings, v.logger)
-	if err != nil {
-		return errors.Wrap(err, "Failed to connect with compass service")
-	}
-	defer compassSvc.CloseConnection() //nolint
-
-	// 1. call compass
-	ctx, err := compassSvc.AuthenticateCompass(c.Context())
-	if err != nil {
-		return errors.Wrap(err, "Failed to authenticate with compass service")
-	}
-	statuses, err := compassSvc.AddVINs(ctx, payload.VINs, payload.Email)
-	if err != nil {
-		return errors.Wrap(err, "Failed to add vehicles to compass service")
-	}
-	for _, status := range statuses {
-		if status.Status == "APPROVED" { // status come from nativeconnect.pb.go enum
-			v.logger.Info().Msgf("Vehicle %s added to Compass", status.VIN)
-			// decode vin
-			// mint + sacd
-			// synthetic device
-		} else {
-			v.logger.Warn().Msgf("Vehicle %s failed to add to Compass: %s", status.VIN, status.Status)
-		}
-	}
-
-	return c.JSON(statuses)
-}
-
-func (v *VehiclesController) DecodeVIN(c *fiber.Ctx) error {
-	payload := DecodeVINRequest{}
-	err := c.BodyParser(&payload)
-	if err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, errors.Wrap(err, "Invalid request body").Error())
-	}
-	targetURL := v.settings.DeviceDefinitionsAPIURL.JoinPath("/device-definitions/decode-vin")
-	b := []byte(`{"vin": "` + payload.VIN + `", "countryCode": "USA"}`)
-
-	return v.proxyRequest(c, targetURL, b, false)
-}
-
-func (v *VehiclesController) GetDefinitionByID(c *fiber.Ctx) error {
-	definitionID := c.Params("id", "")
-
-	definition, err := v.identityAPI.GetDefinitionByID(definitionID)
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, errors.Wrap(err, "Failed to get definition").Error())
-	}
-
-	c.Status(fiber.StatusOK)
-	c.Set("Content-Type", "application/json")
-	// return the exact same JSON response
-	return c.Send(definition)
-
-}
-
-type AddVehicleRequest struct {
-	VINs  []string `json:"vins"`
-	Email string   `json:"email"`
-}
-
-type DecodeVINRequest struct {
-	VIN string `json:"vin"`
 }
