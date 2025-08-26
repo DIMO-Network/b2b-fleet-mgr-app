92acb5129ea8ca1cd869fd5014a2e85e8b849793
James Reategui <JamesReate@users.noreply.github.com>
2025-08-26 11:08:14 -0400

    backend add kaufmann (#40)

--- api/internal/config/settings.go
+++ api/internal/config/settings.go
@@ -3,21 +3,22 @@ package config
 import "net/url"
 
 type Settings struct {
-	Environment        string  `yaml:"ENVIRONMENT"`
-	UseDevCerts        bool    `yaml:"USE_DEV_CERTS"`
-	APIPort            int     `yaml:"API_PORT"`
-	MonitoringPort     int     `yaml:"MONITORING_PORT"`
-	MotorqOracleAPIURL url.URL `yaml:"MOTORQ_ORACLE_API_URL"`
-	StaexOracleAPIURL  url.URL `yaml:"STAEX_ORACLE_API_URL"`
-	IdentityAPIURL     url.URL `yaml:"IDENTITY_API_URL"`
+	Environment          string  `yaml:"ENVIRONMENT"`
+	UseDevCerts          bool    `yaml:"USE_DEV_CERTS"`
+	APIPort              int     `yaml:"API_PORT"`
+	MonitoringPort       int     `yaml:"MONITORING_PORT"`
+	MotorqOracleAPIURL   url.URL `yaml:"MOTORQ_ORACLE_API_URL"`
+	StaexOracleAPIURL    url.URL `yaml:"STAEX_ORACLE_API_URL"`
+	KaufmannOracleAPIURL url.URL `yaml:"KAUFMANN_ORACLE_API_URL"`
 
-	PaymasterURL  url.URL `yaml:"PAYMASTER_URL"`
-	RPCURL        url.URL `yaml:"RPC_URL"`
-	BundlerURL    url.URL `yaml:"BUNDLER_URL"`
-	TurnkeyOrgID  string  `yaml:"TURNKEY_ORG_ID"`
-	TurnkeyAPIURL url.URL `yaml:"TURNKEY_API_URL"`
-	TurnkeyRPID   string  `yaml:"TURNKEY_RP_ID"`
+	IdentityAPIURL url.URL `yaml:"IDENTITY_API_URL"`
+	PaymasterURL   url.URL `yaml:"PAYMASTER_URL"`
+	RPCURL         url.URL `yaml:"RPC_URL"`
+	BundlerURL     url.URL `yaml:"BUNDLER_URL"`
+	TurnkeyOrgID   string  `yaml:"TURNKEY_ORG_ID"`
+	TurnkeyAPIURL  url.URL `yaml:"TURNKEY_API_URL"`
 
+	TurnkeyRPID    string  `yaml:"TURNKEY_RP_ID"`
 	JwtKeySetURL   url.URL `yaml:"JWT_KEY_SET_URL"`
 	ClientID       string  `yaml:"CLIENT_ID"`
 	LoginURL       url.URL `yaml:"LOGIN_URL"`
@@ -39,6 +40,11 @@ func (s *Settings) GetOracles() []Oracle {
 			Name:     "Staex",
 			OracleID: "staex",
 			URL:      s.StaexOracleAPIURL,
+		},
+		{
+			Name:     "Kaufmann",
+			OracleID: "kaufmann",
+			URL:      s.KaufmannOracleAPIURL,
 		},
 	}
 }
